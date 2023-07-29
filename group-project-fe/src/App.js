62481b66a006cd1f056a4fda3b114c7f81e008c9
sayduruddin <saydur.uddin@sky.uk>
2023-07-29 19:21:51 +0100

    Began work on movies component

--- group-project-fe/src/App.js
+++ group-project-fe/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Footer from './Components/Footer';
-import {BrowserRouter, ReactRouter} from 'react-router-dom'
+import "./App.css";
+import Footer from "./Components/Footer";
+import Movies from "./Components/Movies";
+import { BrowserRouter, ReactRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-        {/* Please add components here */}
-
-        <div style={{height: "100vh", textAlign: "center"}}>Random space to separate footer from top of page, will be deleted once components are added</div>
-        <Footer />
-    </BrowserRouter>      
+      {/* Please add components here */}
+      <Movies />
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ group-project-fe/src/Components/Movies.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Movies = () => {
+  return <>Movies within their own cards should show up here</>;
+};
+
+export default Movies;
