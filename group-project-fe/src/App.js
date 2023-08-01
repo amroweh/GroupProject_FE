b4e625f96c0de648c798b3db65e746d1c8470f44
Ali Mroweh <amroweh@gmail.com>
2023-08-01 11:44:00 +0100

    added header

--- group-project-fe/src/App.js
+++ group-project-fe/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Header from "./Components/Header";
 import Movies from "./Components/Movies";
 import Footer from "./Components/Footer";
 import { BrowserRouter, Switch, Routes, Route } from "react-router-dom";
@@ -13,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       {/* <Movies /> */}
-
+      <Header />
       <Routes>
         <Route path="/" element={<Movies />} />
         <Route path="/movie" element={<MoviePage movieTitle="Guardians of the Galaxy"/>} />
