5902c87a7f3dfb49254fb5bf31de045207e60aa8
Jack Wright <jack.wright2@sky.uk>
2023-08-01 17:15:38 +0100

    login barebones

--- group-project-fe/src/App.js
+++ group-project-fe/src/App.js
@@ -6,15 +6,37 @@ import Movies from "./Components/Movies";
 import Footer from "./Components/Footer";
 import { BrowserRouter, Switch, Routes, Route } from "react-router-dom";
 import MoviePage from "./Components/MoviePage"
+import { useEffect } from "react";
+import jwt_decode from "jwt-decode"
+
+const google = window.google;
 
 function App() {
 
-  
+
+  function handleCallbackResponse(response){
+    console.log("Encoded JWT ID:" + response.credential);
+    var userObject = jwt_decode(response.credential)
+    console.log(userObject)
+  }
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id: "205852275312-07o8o2jk2cfd3dtad0m7h3m2c1s3piq4.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+  },[]);
 
   return (
     <BrowserRouter>
       {/* <Movies /> */}
       <Header />
+      <div id="signInDiv"></div>
       <Routes>
         <Route path="/" element={<Movies />} />
         <Route path="/movie" element={<MoviePage movieTitle="Guardians of the Galaxy"/>} />
