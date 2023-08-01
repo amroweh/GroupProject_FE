b2ca6990e29516c69dcccf1f9936b69c50f8e96f
sayduruddin <saydur.uddin@sky.uk>
2023-08-01 16:24:47 +0100

    Fixed images resizing on the carousel

---
+++ group-project-fe/src/tests/Movies.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { create } from "react-test-renderer";
+
+describe("Movie component Test Suite", () => {
+  test("it should render a correct Movie component and display it in cards", () => {
+    console.log("Basic test, not yet implemented");
+  });
+});
